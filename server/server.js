f268819dc49efefccf7c1a3c84110c0b2bcdaff8
Shrinivasuche <shrinivasuche@gmail.com>
2025-07-08 14:48:36 +0530

    Car_Rental

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import "dotenv/config.js";
+import cors from "cors";
+import connectDB from './config/db.js';
+import userRouter from './routes/userRoutes.js';
+import ownerRouter from './routes/ownerRoutes.js';
+import bookingRouter from './routes/bookingRoute.js';
+
+// Initialize Express App
+const app = express();
+// connect database
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>res.send("Server is running"));
+app.use('/api/user', userRouter);
+app.use('/api/owner', ownerRouter);
+app.use('/api/bookings', bookingRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
